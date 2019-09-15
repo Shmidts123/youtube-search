6bbdeeeeb19d2b1e727e13accf796430ea026f1b
Shmidts123 <55348553+Shmidts123@users.noreply.github.com>
2019-09-15 13:07:08 +0200

    Update content.js

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -2,4 +2,37 @@
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('script.js');
 (document.head || document.documentElement).appendChild(s);
-
+{
+  "name": "Youtube text search",
+  "version": "1.0.3",
+  "manifest_version": 2,
+  "description": "Youtube text search",
+  "background": {
+    "scripts": ["background.js"]
+  },
+  "content_scripts": [{
+    "matches": ["*://*.youtube.com/watch*"],
+    "js":      ["content.js"],
+    "run_at":    "document_start"
+  }],
+  "browser_action": {
+    "default_icon": "icon.png",
+    "default_title": "Allow-Control-Allow-Origin",
+    "default_popup": "popup.html"
+  },
+  "permissions": [
+    "storage",
+    "webRequest",
+    "webRequestBlocking",
+    "<all_urls>"
+  ],
+  "web_accessible_resources": ["script.js"]
+}
+{
+  "web_accessible_resources": [
+    "images/*.png",
+    "style/double-rainbow.css",
+    "script/double-rainbow.js",
+    "script/main.js",
+    "templates/*"
+  ],
